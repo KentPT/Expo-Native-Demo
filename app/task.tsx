d6ebe7e4b792cd3f81dbe812e61e5f007a55673f
ponyp <official.kentpinero@gmail.com>
2025-12-01 20:23:41 +0800

    first commit

--- app/task.tsx
+++ app/task.tsx
@@ -1,17 +1,21 @@
-import { View, Text, TextInput, Button, TouchableOpacity, Alert, FlatList } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Alert, FlatList, Image } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { supabase } from '@/services/supabase'
+import Video from 'react-native-video';
 
 interface Task {
     id: number
     title: string
     description: string
+    video_url: string
+    image_url: string
 }
 
 export default function task() {
     const [newTitle, setNewTitle] = useState('');
     const [newDesc, setNewDesc] = useState('');
     const [getTask, setGetTask] = useState<Task[]>([]);
+    const [updateDesc, setUpdateDesc] = useState('');
 
     const handleSubmit = async () => {
 
@@ -24,7 +28,7 @@ export default function task() {
             Alert.alert("Error Inserting Data to Supabase")
             return;
         } else {
-            Alert.alert("Successful Send")
+            Alert.alert("Successfully Send")
         }
 
         setNewTitle(newTitle)
@@ -45,6 +49,37 @@ export default function task() {
 
         setGetTask(data);
 
+    }
+
+    const updateTask = async (id: number) => {
+        const {error} = await supabase
+                    .from('tasks')
+                    .update({description: updateDesc})
+                    .eq('id', id)
+
+        if (error) {
+            Alert.alert("Error update task")
+            return;
+        } else {
+            Alert.alert("Successfully updated")
+        }
+
+        setUpdateDesc(updateDesc);
+
+    }
+
+    const deleteTask = async (id: number) => {
+        const { error } = await supabase
+                    .from('tasks')
+                    .delete()
+                    .eq('id', id)
+        
+        if (error) {
+            Alert.alert("Error delete task")
+            return;
+        } else {
+            Alert.alert("Successfully deleted")
+        }
     }
 
     useEffect(() => {
@@ -73,16 +108,6 @@ export default function task() {
     <TouchableOpacity className='w-[200px] m-3 p-4 bg-blue-600' onPress={handleSubmit}>
         <Text className='text-white'>Submit</Text>
     </TouchableOpacity>
-
-        {/* {getTask.map((task, key) => (
-            // <View className="bg-lime-200 p-4 m-4"key={key}>
-            //     <Text className='text-black'>{task.title}</Text>
-            //     <Text className='text-black'>{task.description}</Text>
-            // </View>
-            
-        ))} */}
-
-
         {/* Use Flatlist instead for reliability */}
          <FlatList
             data={getTask}
@@ -91,6 +116,27 @@ export default function task() {
                 <View className='bg-slate-700 p-4 m-4'>
                     <Text className='text-white'>{item.title}</Text>
                     <Text className='text-white'>{item.description}</Text>
+                    <Image className="h-20 w-20" src={item.image_url}/>
+
+
+                    <Text className='text-white'>Update Description</Text>
+                    <TextInput 
+                    className='w-[200px] m-4 p-4 bg-slate-100'
+                    placeholder='Description'
+                    value={updateDesc}
+                    onChangeText={setUpdateDesc}
+                    />
+
+                    <Video source={{ uri: item.video_url }} controls/>
+
+                    <TouchableOpacity className='w-[200px] m-3 p-4 bg-yellow-600' onPress={() => updateTask(item.id)}>
+                        <Text className='text-white'>Update</Text>
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity className='w-[200px] m-3 p-4 bg-red-600' onPress={() => deleteTask(item.id)}>
+                        <Text className='text-white'>Delete</Text>
+                    </TouchableOpacity>
                 </View>
             )}
             />
